2cf0bf372a145e705821735d6da1eef989a025bf
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-09 18:58:20 -0400

    added retrieval of local storage for persistance

--- assets/script.js
+++ assets/script.js
@@ -47,9 +47,20 @@ $(document).ready(function () {
     // Get div id value
     var divID = $(this).parent().attr('id')
 
-    // Place user input into storage using key value of divID and text value of text
+    // Store user input using key value of divID and text value of text
     localStorage.setItem(divID, task)
   })
 
+  // Load saved data
+  $('#9 .description').val(localStorage.getItem('9'))
+  $('#10 .description').val(localStorage.getItem('10'))
+  $('#11 .description').val(localStorage.getItem('11'))
+  $('#12 .description').val(localStorage.getItem('12'))
+  $('#13 .description').val(localStorage.getItem('13'))
+  $('#14 .description').val(localStorage.getItem('14'))
+  $('#15 .description').val(localStorage.getItem('15'))
+  $('#16 .description').val(localStorage.getItem('16'))
+  $('#17 .description').val(localStorage.getItem('17'))
+  
 });
 
