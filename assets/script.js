0f31662d2d3bfd8422b66701c83a6a94688d27fb
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-09 12:32:43 -0400

    added current date to top of day planner

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
-    function updateTime() {
-      var currentTime = date.currentTime()
-    }
-  }
+
+  // Get current time
+  var currentDateAndTime = Date(Date.now()); // exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
+  var currentHour = new Date().getHours(); //Current hour in military time
+  console.log(currentDateAndTime)
+  console.log(currentHour)
+  console.log(typeof currentHour)
+
+  // Set the current date & time (#currentDay)
+  $('#currentDay').text(currentDateAndTime)
+
+  
+
+  // Listener for the save buttons
+
+});
+
+
+
+
+
