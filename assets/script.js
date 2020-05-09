ccc447672d80742bd582a00ded50fb6e572c8249
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-09 17:34:28 -0400

    restructure the current date from full date and time to just the weekday, month, and numeric day

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,15 @@
+// Wrapped jQuery code in document ready to ensure that all HTML is loaded prior to running the jQuery code
 $(document).ready(function () {
 
-  // Get current time
-  var currentDateAndTime = Date(Date.now()); // exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
-  var currentHour = new Date().getHours(); //Current hour in military time
-  console.log(currentDateAndTime)
-  console.log(currentHour)
-  console.log(typeof currentHour)
+  // Get current time and date variables
+  var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+  var day = new Date().getDay()
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  var numMonth = new Date().getMonth()
+  var currentHour = new Date().getHours()
 
   // Set the current date & time (#currentDay)
-  $('#currentDay').text(currentDateAndTime)
-
-  
-
-  // Listener for the save buttons
+  $('#currentDay').text(weekday[day] + ', ' + months[numMonth] + ' ' + day)
 
 });
-
-
-
-
 
