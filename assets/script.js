1e1473b96a8bb00d98ec81e27fe47e06b71d144c
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-09 17:48:43 -0400

    added the current time loop function to update background colors

--- assets/script.js
+++ assets/script.js
@@ -6,10 +6,34 @@ $(document).ready(function () {
   var day = new Date().getDay()
   var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   var numMonth = new Date().getMonth()
-  var currentHour = new Date().getHours()
 
-  // Set the current date & time (#currentDay)
+  // Set the current date (#currentDay)
   $('#currentDay').text(weekday[day] + ', ' + months[numMonth] + ' ' + day)
+
+  // Current time loop function to update background colors
+  function updateTimeBlock() {
+
+    // Loop over all the .time-blocks to find current time
+    $('.time-block').each(function () {
+
+      // Get the current hour
+      var currentHour = new Date().getHours()
+
+      // Get the div's id value
+      var divID = parseInt($(this).attr('id'))
+
+      // Compare div's id value to the current hour
+      if (divID === currentHour) {
+        $(this).addClass('present')
+      } else if (divID < currentHour) {
+        $(this).addClass('past')
+      } else {
+        $(this).addClass('future')
+      }
+    })
+  }
+
+  updateTimeBlock()
 
 });
 
